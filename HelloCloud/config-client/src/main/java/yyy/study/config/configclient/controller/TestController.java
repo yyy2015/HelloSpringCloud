1cd3f90d921df9cddff4e0465acb3a2a9d435abb
yyy2015 <2456700983@qq.com>
2018-10-29 14:12:37 +0800

    add: config

---
+++ HelloCloud/config-client/src/main/java/yyy/study/config/configclient/controller/TestController.java
@@ -0,0 +1,20 @@
+package yyy.study.config.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created on 2018/10/27
+ */
+@RestController
+public class TestController {
+
+    @Value("${foo}")
+    String foo;
+
+    @GetMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
