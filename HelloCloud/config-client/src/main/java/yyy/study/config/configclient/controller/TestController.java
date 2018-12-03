082d7651b11b4fe5c2d96c273f127476f0d3b722
yyy2015 <2456700983@qq.com>
2018-12-03 22:39:23 +0800

    spring cloud bus + rabbitmq

--- HelloCloud/config-client/src/main/java/yyy/study/config/configclient/controller/TestController.java
+++ HelloCloud/config-client/src/main/java/yyy/study/config/configclient/controller/TestController.java
@@ -1,6 +1,7 @@
 package yyy.study.config.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created on 2018/10/27
  */
 @RestController
+@RefreshScope
 public class TestController {
 
     @Value("${foo}")
---
+++ RabbitmqDemo/src/main/java/Worker.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * Created on 2018/12/3
+ */
+public class Worker {
+
+}
